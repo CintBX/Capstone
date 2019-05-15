c8a85df9f5d798b319e74f50aaade9658dae167c
Dustin Cintron <D.Cint88@gmail.com>
2019-05-15 19:11:47 -0400

    Implemented image defaults for Portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,10 @@
 class Portfolio < ApplicationRecord
 	validates_presence_of :title, :body
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "https://via.placeholder.com/350"
+		self.thumb_image ||= "https://via.placeholder.com/150"
+	end
 end
