67c7af16883b6e07317586352df1c8b041d3d724
Dustin Cintron <D.Cint88@gmail.com>
2019-05-15 18:42:08 -0400

    Basic set up for Home, About, and Portfolio pages

---
+++ app/controllers/portfolios_controller.rb
@@ -0,0 +1,61 @@
+class PortfoliosController < ApplicationController
+	before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
+	def index
+		@portfolios = Portfolio.all
+	end
+
+	def new
+		@portfolio = Portfolio.new
+	end
+
+	def create
+		@portfolio = Portfolio.new(portfolio_params)
+
+		respond_to do |format|
+			if @portfolio.save
+				format.html { redirect_to portfolios_path, notice: "Your portfolio has been created." }
+			else
+				format.html { render :new }
+			end
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		respond_to do |format|
+			if @portfolio.update(portfolio_params)
+				format.html { redirect_to portfolios_path, notice: "Your portfolio has been updated." }
+			else
+				format.html { render :edit }
+			end
+		end
+	end
+
+	def destroy
+		@portfolio.destroy
+
+		respond_to do |format|
+			format.html { redirect_to portfolios_path, notice: "Record was deleted." }
+		end
+	end
+
+	private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      :main_image,
+                                      :thumb_image
+                                      )
+  end
+
+  def set_portfolio_item
+    @portfolio = Portfolio.find(params[:id])
+  end
+end
