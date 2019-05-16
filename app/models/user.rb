5101e738816a4e57b8341e837cb060feb8b92ba5
Dustin Cintron <D.Cint88@gmail.com>
2019-05-15 22:21:26 -0400

    Added name attribute to user model and schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-	attr_accessor :first_name, :last_name
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+ validates_presence_of :name
 end
