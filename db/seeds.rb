6be658fd097e0e9b073b47ed2911d969374d376f
Dustin Cintron <D.Cint88@gmail.com>
2019-05-15 22:21:26 -0400

    Added name attribute to user model and schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,6 @@
 
 puts "User created."
 
-5.times do |blog|
-	Blog.create!(
-		title: "Blog #{blog}",
-		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-	)
-end
-
-puts "5 Blogs created."
-
 10.times do |portfolio|
 	Portfolio.create!(
 		title: "Portfolio #{portfolio}",
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,17 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 	before do
-		@user = User.create(first_name: "Jon", last_name: "Snow", email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+		@user = User.create(name: "Jon Snow", email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
 	end
 
 	it 'can be created' do
 		expect(@user).to be_valid
 	end
+
+  it 'cannot be created without a name' do
+    @user.name = nil
+    expect(@user).to_not be_valid
+  end
 
   it 'cannot be created without an email' do
     @user.email = nil
