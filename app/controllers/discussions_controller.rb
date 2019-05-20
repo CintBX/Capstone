0be12a1771d54cfa6f7bca59ad8246669ab3f72b
Dustin Cintron <D.Cint88@gmail.com>
2019-05-20 16:32:22 -0400

    Formatted replies controller, cleaned up comments in channels and discussions controllers

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -2,29 +2,21 @@ class DiscussionsController < ApplicationController
   before_action :set_discussion, only: [:show, :edit, :update, :destroy]
   before_action :find_channels, only: [:index, :show, :new, :edit]
 
-  # GET /discussions
-  # GET /discussions.json
   def index
     @discussions = Discussion.all.order("created_at DESC")
   end
 
-  # GET /discussions/1
-  # GET /discussions/1.json
   def show
     @discussions = Discussion.all.order("created_at DESC")
   end
 
-  # GET /discussions/new
   def new
     @discussion = current_user.discussions.build
   end
 
-  # GET /discussions/1/edit
   def edit
   end
 
-  # POST /discussions
-  # POST /discussions.json
   def create
     @discussion = current_user.discussions.build(discussion_params)
 
@@ -39,8 +31,6 @@ class DiscussionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /discussions/1
-  # PATCH/PUT /discussions/1.json
   def update
     respond_to do |format|
       if @discussion.update(discussion_params)
@@ -53,8 +43,6 @@ class DiscussionsController < ApplicationController
     end
   end
 
-  # DELETE /discussions/1
-  # DELETE /discussions/1.json
   def destroy
     @discussion.destroy
     respond_to do |format|
@@ -64,12 +52,10 @@ class DiscussionsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_discussion
       @discussion = Discussion.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def discussion_params
       params.require(:discussion).permit(:title, :content)
     end
