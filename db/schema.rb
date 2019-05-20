854bc1062198fe140c717a0a2412c371039486ac
Dustin Cintron <D.Cint88@gmail.com>
2019-05-20 14:11:21 -0400

    Wired up model relationships among user, channel, discussion, and reply

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_174555) do
+ActiveRecord::Schema.define(version: 2019_05_20_174825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_05_20_174555) do
     t.string "channel"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "discussion_id"
   end
 
   create_table "discussions", force: :cascade do |t|
@@ -27,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_05_20_174555) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "channel_id"
   end
 
   create_table "portfolios", force: :cascade do |t|
