f9a2e945afb379c5062b6a6c358d5455f4115e54
Dustin Cintron <D.Cint88@gmail.com>
2019-05-20 13:34:48 -0400

    Added replies to discussion routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+ # Possibly validate :username
  validates_presence_of :name
 
  def first_name
