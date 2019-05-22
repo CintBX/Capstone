14471bd57a0a038930a068f07d5be8b02953666d
Dustin Cintron <D.Cint88@gmail.com>
2019-05-22 12:45:08 -0400

    Created helper method has_role? for admin role check in edit user form

--- app/helpers/discussions_helper.rb
+++ app/helpers/discussions_helper.rb
@@ -1,5 +1,4 @@
 module DiscussionsHelper
-	# Markdown
 	require 'redcarpet/render_strip'
 
 	class CodeRayify < Redcarpet::Render::HTML
@@ -8,25 +7,27 @@ module DiscussionsHelper
 		end
 	end
 
-	def markdown(text)
-		coderayified = CodeRayify.new(:filter_html => true, :hard_wrap => true)
-		options = {
-			fenced_code_blocks: true,
-			no_intra_emphasis: true,
-			autolink: true,
-			lax_html_blocks: true
-		}
-		markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
-		markdown_to_html.render(text).html_safe
+		def markdown(text)
+			coderayified = CodeRayify.new(:filter_html => true, :hard_wrap => true)
+			options = {
+				fenced_code_blocks: true,
+				no_intra_emphasis: true,
+				autolink: true,
+				lax_html_blocks: true
+			}
+			markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+			markdown_to_html.render(text).html_safe
+		end
+
+		def strip_markdown(text)
+			markdown_to_plain_text = Redcarpet::Markdown.new(Redcarpet::Render::StripDown)
+			markdown_to_plain_text.render(text).html_safe
+		end
+
+	def has_role?(role)
+		current_user && current_user.has_role?(role)
 	end
 
-	def strip_markdown(text)
-		markdown_to_plain_text = Redcarpet::Markdown.new(Redcarpet::Render::StripDown)
-		markdown_to_plain_text.render(text).html_safe
-	end
-
-
-	# User role checking
 	def discussion_author(discussion)
 		user_signed_in? && current_user.id == discussion.user_id
 	end
