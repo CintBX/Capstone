0f0e54cfc6ba67944dcc4fdcc0e05259cd078345
Dustin Cintron <D.Cint88@gmail.com>
2019-05-21 23:05:29 -0400

    Removed user concern containing guest_user code.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,5 @@ class ApplicationController < ActionController::Base
 	# before_action :authenticate_user!
 	protect_from_forgery with: :exception
 	include DeviseWhitelist
-	include CurrentUserConcern
 	include DefaultPageContent
 end
