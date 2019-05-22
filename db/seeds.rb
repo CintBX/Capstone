7db0de1a29abc509b31c8c551aa685bc6199d93f
Dustin Cintron <D.Cint88@gmail.com>
2019-05-22 12:25:09 -0400

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
-@user = User.create(name: "Dustin Cintron", username: "DCint", email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+@admin_user = User.create(name: "Dustin Cintron", username: "DCin", email: "d.cint88@gmail.com", password: "password", password_confirmation: "password")
+puts "1 Admin created."
 
-puts "User created."
+@user = User.create(name: "Jon Doe", username: "JD", email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+puts "1 User created."
 
 10.times do |portfolio|
 	Portfolio.create!(
@@ -11,5 +13,23 @@ puts "User created."
 		thumb_image: "https://via.placeholder.com/150"
 	)
 end
-
 puts "10 Portfolios created."
+
+@channel_one = Channel.create!(channel: "General Topics")
+@channel_two = Channel.create!(channel: "Software Development")
+@channel_three = Channel.create!(channel: "Entertainment")
+puts "3 Channels created."
+
+Discussion.create!(title: "Latin Text",
+									content: "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from 'de Finibus Bonorum et Malorum' by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
+									user_id: @admin_user.id,
+									channel_id: @channel_one.id)
+Discussion.create!(title: "Ruby on Rails",
+									content: "I am working on my Capstone project to become a professional Ruby on Rails developer.",
+									user_id: @admin_user.id,
+									channel_id: @channel_two.id)
+Discussion.create!(title: "Game of Thrones",
+									content: "I am disappointed in the ending.  Does anyone else feel this way?",
+									user_id: @user.id,
+									channel_id: @channel_three.id)
+puts "3 Discussions created."
