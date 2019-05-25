953f0fb15b0246a8c117047740f57bc107ccef5a
Dustin Cintron <D.Cint88@gmail.com>
2019-05-25 19:31:55 -0400

    Set image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -11,7 +11,7 @@ class Portfolio < ApplicationRecord
 	after_initialize :set_defaults
 
 	def set_defaults
-		self.main_image ||= "https://via.placeholder.com/350"
-		self.thumb_image ||= "https://via.placeholder.com/150"
+		self.main_image ||= "https://via.placeholder.com/600x400"
+		self.thumb_image ||= "https://via.placeholder.com/350x200"
 	end
 end
