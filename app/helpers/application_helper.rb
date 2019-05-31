d2e0cbdb06600827aa5f77fefd903b2d7d9e354b
Dustin Cintron <D.Cint88@gmail.com>
2019-05-31 08:53:30 -0400

    Implemented helper method/refactor for nav bars, added active status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,9 @@ module ApplicationHelper
 		else
 			(link_to 'Login', new_user_session_path, class: style) + 
 			" ".html_safe + 
-  		(link_to 'Register', new_user_registration_path, class: style)
+			unless current_page? portfolios_path
+				(link_to 'Register', new_user_registration_path, class: style)
+			end
 		end
 	end
 
@@ -18,5 +20,39 @@ module ApplicationHelper
 
 	def copyright_generator
 		CintronViewTool::Renderer.copyright 'Dustin Cintron', 'All rights reserved'
+	end
+
+	def nav_items
+		[
+			{
+				url: root_path,
+				title: "Home"
+			},
+			{
+				url: about_me_path,
+				title: "About"
+			},
+			{
+				url: portfolios_path,
+				title: "Portfolio"
+			},
+			{
+				url: discussions_path,
+				title: "Discussion Board"
+			}
+		]
+	end
+
+	def nav_helper(style, tag_type)
+		nav_links = ''
+		nav_items.each do |item|
+			nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+		end
+
+		nav_links.html_safe
+	end
+
+	def active? path
+		"active" if current_page? path
 	end
 end
